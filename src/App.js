10978da72b26e197478195e6c6c23570b4e0aed1
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-10-21 00:27:30 +0530

    write your story page created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TopBar from "./components/Topbar/Topbar";
 // import Home from "./Pages/Home/Home";
-import Single from "./Pages/single/Single";
+// import Single from "./Pages/single/Single";
+import Write from "./Pages/write/Write";
 
 
 function App() {
   return (
     <div className="App">
     <TopBar/>
-    <Single/>
+    <Write/>
     </div>
   );
 }
