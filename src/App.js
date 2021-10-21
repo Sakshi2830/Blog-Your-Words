3cda82c49cdf8839b7d5a5da1d85cbe7d2aa8ae8
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-10-22 02:41:06 +0530

    Adding route to each link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import TopBar from "./components/Topbar/Topbar";
-// import Login from "./Pages/Login/Login";
+import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-// import Setting from "./Pages/setting/Setting";
-// import Home from "./Pages/Home/Home";
-// import Single from "./Pages/single/Single";
-// import Write from "./Pages/write/Write";
+import Setting from "./Pages/setting/Setting";
+import Home from "./Pages/Home/Home";
+import Single from "./Pages/single/Single";
+import Write from "./Pages/write/Write";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
 function App() {
+  const user = true
   return (
-    <div className="App">
-    <TopBar/>
-  <Register/>
-
-    </div>
+    <Router>
+      <TopBar/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/register">
+          {user ? <Home/>:<Register/>}
+        </Route>
+        <Route path="/login">
+          {user ? <Home/>:<Login/>}
+        </Route>
+        <Route path="/write">
+          {user ?<Write/>: <Register/>}
+        </Route>
+        <Route path="/setting">
+          {user ? <Setting/> : <Register/>}
+        </Route>
+        <Route path="/post/:postId">
+          <Single/>
+        </Route> 
+    </Switch>
+    </Router>
   );
 }
 
