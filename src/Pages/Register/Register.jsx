136e52d526a54c70c0f94446ee3904e2cb349cc1
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-10-22 03:04:09 +0530

    adding links and route to pages

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./register.css"
 
 export default function Register() {
@@ -14,7 +15,9 @@ export default function Register() {
             <input type="password" placeholder="Enter your Password" className="registerInput" />
             <button className="registerButton">Register</button>
             </form>
-            <button className="registerLoginButton">Login</button>
+            <button className="registerLoginButton">
+                <Link className="link" to="login">Login</Link>
+            </button>
         </div>
     )
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Topbar.css"
 
 export default function TopBar() {
+    const user = false
     return (
         <div className="top">
             <div className="top_left">
@@ -13,15 +15,38 @@ export default function TopBar() {
             </div>
             <div className="top_center">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">HOME</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">ABOUT</Link></li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">CONTACT</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/write">WRITE</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">
+                        {user && "LOGOUT"}
+                    </Link>
+                    </li>
                 </ul>
             </div>
             <div className="top_right">
+                {user ? (
                 <img className="topImage" src="https://cdn.sharechat.com/profiledp_8c2b507f-1c73-400d-ba99-bcede29a42ee-90a7e13d-59fd-4e82-95dc-98dcc9a0577b_cmprsd_40.jpg" alt="profile" />
+    ) : (
+        <ul className="topList">
+            <li className="topListItem">
+                <Link className="link" to="/login">LOGIN</Link>
+            </li>
+            <li className="topListItem">
+            <Link className="link" to="/register">REGISTER</Link>
+            </li>
+        </ul>
+
+    )}
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
             
